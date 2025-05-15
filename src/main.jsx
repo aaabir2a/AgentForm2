d8a8b8bbae0e7b950c68a00cbed65c8d3e783d5d
aaabir2a <30332689+aaabir2a@users.noreply.github.com>
2025-05-15 20:01:48 +0600

    remove unused theme import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import App from "./App"
-import theme from "./theme"
 import "./index.css"
 
 // Set document title
